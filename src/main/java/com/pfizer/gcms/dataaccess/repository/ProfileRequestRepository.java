7be87c84dac813bed5e99e56cb25e971d9ca17d5
KASWAS <KASWAS@APCW10A6G00BML6.apac.pfizer.com>
2017-09-08 13:14:51 +0530

    Change-Id: Ic8865610a96e6ecc21e61076b6cab9b6e5509fdf

--- src/main/java/com/pfizer/gcms/dataaccess/repository/ProfileRequestRepository.java
+++ src/main/java/com/pfizer/gcms/dataaccess/repository/ProfileRequestRepository.java
@@ -2,6 +2,11 @@ package com.pfizer.gcms.dataaccess.repository;
 
 import java.util.Locale;
 
+import javax.persistence.EntityManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.lang.time.StopWatch;
+
 import com.pfizer.gcms.dataaccess.model.ProfileRequestModel;
 
 /**
@@ -9,6 +14,7 @@ import com.pfizer.gcms.dataaccess.model.ProfileRequestModel;
  * The ProfileRequests Repository will provide methods for accessing ProfileRequests stored in the database.
  */
 public class ProfileRequestRepository extends AbstractRepository<ProfileRequestModel>  {
+	private static final Log LOG = LogFactory.getLog(AbstractRepository.class);
 	/**
 	 * Creates a new instance of the ProfileRequest Repository and configures the abstract class with the correct models.
 	 * 
@@ -18,5 +24,34 @@ public class ProfileRequestRepository extends AbstractRepository<ProfileRequestM
 	public ProfileRequestRepository(Locale country) {
 		super(country);
 		setModelType(ProfileRequestModel.class);
+	}
+	
+	
+	/**
+	 * Updates the current instance of Model and persists the changes to the database.
+	 * @param item
+	 *            (ModelType) – The representation containing the data that was just updated
+	 * @return ModelType
+	 * @throws Exception
+	 *             if save is unsuccessful
+	 */
+	public ProfileRequestModel update(ProfileRequestModel item) throws Exception {
+		LOG.debug("Inside update(ProfileRequestModel item)");
+		
+		StopWatch timer = new StopWatch();
+        timer.start();
+		
+		EntityManager entityManager = getEntityManager();
+		
+		StopWatch timer1 = new StopWatch();
+        timer1.start();
+        
+        ProfileRequestModel result = entityManager.merge(item);
+		
+		LOG.debug("#Performance#Repository#Total time took only for merge during update is - " + timer1.getTime());
+		
+		LOG.debug("#Performance#Repository#Total time took for update(ModelType item) operation is - " + timer.getTime());
+		
+		return result;
 	}
 }
